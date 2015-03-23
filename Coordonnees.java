c0b70be05413e1341de73c875af03dff95c816bc
RST-59 <RST-59@users.noreply.github.com>
2015-03-23 08:56:07 +0100

    Create Coordonnees.java
    j'ai crée ça quand je suis fait c***r alors enjoy

---
+++ Coordonnees.java
@@ -0,0 +1,34 @@
+public class Coordonnees 
+{
+	private int largeur;
+	private int hauteur;
+	
+	public Coordonnees(int largeur, int hauteur) 
+	{
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+	}
+
+	public int getLargeur() 
+	{
+		return largeur;
+	}
+	
+	public int getHauteur() 
+	{
+		return hauteur;
+	}
+	
+	public String toString() 
+	{
+		return "["+largeur+";"+hauteur+"]";
+	}
+	
+	public Coordonnees ajout(Coordonnees coord)
+	{
+		int l = this.largeur + coord.getLargeur();
+		int h = this.hauteur + coord.getHauteur();
+		
+		return new Coordonnees(l, h);
+	}
+}
