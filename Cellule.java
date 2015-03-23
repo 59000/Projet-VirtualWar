2ef46b8703451945c2bb31f4e6c59d15c379a5ed
EiKoZ <quentin.beaudoul-br@laposte.net>
2015-03-23 13:24:58 +0100

    Update Cellule.java

--- Cellule.java
+++ Cellule.java
@@ -1,42 +1,38 @@
-public abstract class Cellule extends Coordonnees 
-{
+public abstract class Cellule extends Coordonnees {
 	protected int mine;
 	protected int base;
 	String image;
 
-	public Cellule(int largeur, int hauteur) 
-	{
+	public Cellule(int largeur, int hauteur) {
 		super(largeur, hauteur);
 	}
 
-	public int contienMine() 
-	{
+	public int contienMine() {
 		return mine;
 	}
 
-	public int estBase() 
-	{
+	public int estBase() {
 		return base;
 	}
 
-	public Coordonnees getCoordonnees() 
-	{
+	public Coordonnees getCoordonnees() {
 		return new Coordonnees(super.getLargeur(), super.getHauteur());
 	}
-	
-	public Robot getContenu()
-	{
-		//je vois pas comment on detecte si return un robot dc ...
+
+	public Robot getContenu() {
+		// je vois pas comment on detecte si return un robot dc ...
 		return null;
 	}
 
 	@Override
-	public String toString() 
-	{
-		return "Cellule [mine=" + mine + ", base=" + base + ", image=" + image+ "]";
+	public String toString() {
+		return "Cellule [mine=" + mine + ", base=" + base + ", image=" + image
+				+ "]";
 	}
-	
+
 	abstract void deplaceSur(Robot robot);
+
 	abstract void ajout(int equipes);
+
 	abstract void videCase();
 }
