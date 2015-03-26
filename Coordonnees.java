2f8a0b8de65a14150d622c4a0bce4439e471f526
Sawah <rayanlesage@gmail.com>
2015-03-26 14:53:04 +0100

    Update Coordonnees.java

--- Coordonnees.java
+++ Coordonnees.java
@@ -1,48 +1,40 @@
-public class Coordonnees 
-{
+public class Coordonnees {
 	private int largeur;
 	private int hauteur;
-	
-	public Coordonnees(int largeur, int hauteur) 
-	{
+
+	public Coordonnees(int largeur, int hauteur) {
 		this.largeur = largeur;
 		this.hauteur = hauteur;
 	}
-	
-	/**retourne la composante x*/
-	public int getLargeur() 
-	{
+
+	/** retourne la composante x */
+	public int getLargeur() {
 		return largeur;
 	}
-	/**retourne la composante y*/
-	public int getHauteur() 
-	{
+
+	/** retourne la composante y */
+	public int getHauteur() {
 		return hauteur;
 	}
-	
-	/**retourne les coordonnée sous la forme "[x;y]"*/
-	public String toString() 
-	{
-		return "["+largeur+";"+hauteur+"]";
+
+	/** retourne les coordonnée sous la forme "[x;y]" */
+	public String toString() {
+		return "[" + this.largeur + ";" + this.hauteur + "]";
 	}
-	
-	/**ajoute des coordonnées aux coordonnées courantes*/
-	public Coordonnees ajout(Coordonnees coord)
-	{
-		int l = this.largeur + coord.getLargeur();
-		int h = this.hauteur + coord.getHauteur();
-		
+
+	/** ajoute des coordonnées aux coordonnées courantes */
+	public Coordonnees ajout(Coordonnees coord) {
+		int l = (this.largeur + coord.getLargeur());
+		int h = (this.hauteur + coord.getHauteur());
+
 		return new Coordonnees(l, h);
 	}
-	
-	
-	
-	/**soustrait des coordonnées aux coordonnées courantes*/
-	public Coordonnees soustrait(Coordonnees coord)
-	{
-		int l = this.largeur - coord.getLargeur();
-		int h = this.hauteur - coord.getHauteur();
-		
+
+	/** soustrait des coordonnées aux coordonnées courantes */
+	public Coordonnees soustrait(Coordonnees coord) {
+		int l = (this.largeur - coord.getLargeur());
+		int h = (this.hauteur - coord.getHauteur());
+
 		return new Coordonnees(l, h);
 	}
 }
