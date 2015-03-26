1988d2935aba01aeaa372df9c23c101b1f034d68
RST-59 <RST-59@users.noreply.github.com>
2015-03-26 08:43:40 +0100

    Update Coordonnees.java
    ajout commentaires

--- Coordonnees.java
+++ Coordonnees.java
@@ -1,28 +1,37 @@
-public class Coordonnees {
+public class Coordonnees 
+{
 	private int largeur;
 	private int hauteur;
-
-	public Coordonnees(int largeur, int hauteur) {
+	
+	public Coordonnees(int largeur, int hauteur) 
+	{
 		this.largeur = largeur;
 		this.hauteur = hauteur;
 	}
-
-	public int getLargeur() {
+	
+	/**retourne la composante x*/
+	public int getLargeur() 
+	{
 		return largeur;
 	}
-
-	public int getHauteur() {
+	/**retourne la composante y*/
+	public int getHauteur() 
+	{
 		return hauteur;
 	}
-
-	public String toString() {
-		return "[" + largeur + ";" + hauteur + "]";
+	
+	/**retourne les coordonnée sous la forme "[x;y]"*/
+	public String toString() 
+	{
+		return "["+largeur+";"+hauteur+"]";
 	}
-
-	public Coordonnees ajout(Coordonnees coord) {
+	
+	/**ajoute des coordonnées aux coordonnées courantes*/
+	public Coordonnees ajout(Coordonnees coord)
+	{
 		int l = this.largeur + coord.getLargeur();
 		int h = this.hauteur + coord.getHauteur();
-
+		
 		return new Coordonnees(l, h);
 	}
 }
