0fd973a5de2c338e6b0888b23b0b841e2396c1d3
RST-59 <RST-59@users.noreply.github.com>
2015-03-26 10:21:44 +0100

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,3 +1,4 @@
+
 public abstract class Robot 
 {
 	private Coordonnees coord;
@@ -8,7 +9,7 @@ public abstract class Robot
 	{
 		setCoord(new Coordonnees(0, 0));
 		this.equipe = equipe;
-		this.energie = energie;
+		this.setEnergie(energie);
 	}
 	
 	/** deplace le robot selon une diréction*/
@@ -17,7 +18,7 @@ public abstract class Robot
 	/**augmente l'energie du robot en base de 2*/
 	void regeneration()
 	{
-		energie += this.getCoord().equals(new Coordonnees(0, 0)) ? 2:0;//pour la compréhansion de tous : "identique aux lignes du dessou"
+		setEnergie(getEnergie() + (this.getCoord().equals(new Coordonnees(0, 0)) ? 2:0));//pour la compréhansion de tous : "identique aux lignes du dessou"
 		/*if(this.getCoord().equals(new Coordonnees(0, 0)))
 		{
 			energie+=2;
@@ -25,14 +26,26 @@ public abstract class Robot
 	}
 	
 	/**diminue l'energie du robot*/
-	void dégat(int valeur)
-	{
-		energie -= valeur;
-	}
+	abstract void dégat(String cause);
+	
 	
 	/**attaque la cible de coordonnée c*/
 	abstract boolean attaque(Coordonnees c);
-
+	
+	/**attaque la cible*/
+	boolean attaque(Robot cible)
+	{
+		if(verifier_portée(cible.getCoord()))
+		{
+			cible.dégat("cible");
+			return true;
+		}
+		
+		return false;
+	}
+	
+	abstract protected boolean verifier_portée(Coordonnees c);
+	
 	public Coordonnees getCoord() 
 	{
 		return coord;
@@ -41,6 +54,14 @@ public abstract class Robot
 	public void setCoord(Coordonnees coord) 
 	{
 		this.coord = coord;
+	}
+
+	public int getEnergie() {
+		return energie;
+	}
+
+	public void setEnergie(int energie) {
+		this.energie = energie;
 	}
 	
 }
