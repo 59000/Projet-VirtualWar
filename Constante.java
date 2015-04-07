4c5c34295ff8a0a06643ddba00efe1103dc618db
EiKoZ <quentin.beaudoul-br@laposte.net>
2015-04-07 13:35:50 +0200

    Create Constante.java

---
+++ Constante.java
@@ -0,0 +1,38 @@
+public class Constante {
+	
+	public static final int PORTEETIREUR = 3;
+	public static final int PORTEEPIEGEUR = 1;
+	public static final int PORTEECHAR = 10;
+	
+	
+	public static final int DEPLACEMENTTIREUR = 1;
+	public static final int DEPLACEMENTPIEGEUR = 1;
+	public static final int DEPLACEMENTCHAR = 1;
+	
+	
+	
+	public static final int ENERGIETIREUR = 40;
+	public static final int ENERGIEPIEGEUR = 50;
+	public static final int ENERGIECHAR = 60;
+	
+	
+	
+	public static final int REGENENBASE = 2;
+	
+	
+	public static final int COUTMINER = -2;
+	public static final int COUTTIRERTIREUR = -2;
+	public static final int COUTTIRERCHAR = 1;
+	public static final int COUTAVANCERTIREUR = -1;
+	public static final int COUTAVANCERPIEGEUR = -5;
+	public static final int COUTAVANCERCHAR = -5;
+	
+	
+	
+	public static final int DEGATTIREUR = -3;
+	public static final int DEGATPIEGEUR = -2;
+	public static final int DEGATCHAR = 6;
+
+	
+
+}
