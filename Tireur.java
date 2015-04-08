26fe91c492aaae67c61125c75b483df39ffa6256
RST-59 <RST-59@users.noreply.github.com>
2015-04-08 09:04:45 +0200

    Update Tireur.java

--- Tireur.java
+++ Tireur.java
@@ -1,80 +1,45 @@
+import java.util.List;
+
 public class Tireur extends Robot {
-	/*Supprimer les methodes inutiles et utiliser les methodes Attaque et deplacement */
-	public Tireur(int equipe,int numero) {
+
+	public Tireur(int equipe,int numero) {/*implémenter*/
 		super(equipe,numero, 40);
 	}
 
-	/**
-	 * direction selon le pavé numérique (5 est le centre) 2 la bas , 8 le
-	 * haut...
-	 */
-	void deplacement(int direction) {
-		switch (direction) {
-		case 1:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(-1, -1)));// bas gauche
-			break;
-		case 2:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(0, -1)));// bas
-			break;
-		case 3:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(1, -1)));// bas droite
-			break;
-		case 4:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(-1, 0)));// gauche
-			break;
-		case 6:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(1, 0)));// droite
-			break;
-		case 7:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(-1, 1)));// haut gauche
-			break;
-		case 8:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(0, 1)));// haut
-			break;
-		case 9:
-			super.setCoord(super.getCoord().ajout(new Coordonnees(1, 1)));// haut droite
-			break;
-
-		default:
-
-			break;
-		}
-	}
-
-	protected boolean verifier_portee(Coordonnees c) {
-		Coordonnees resultat = super.getCoord().soustrait(c);
-		boolean portee_haute = (resultat.getHauteur() <= 3 && resultat.getLargeur() == 0) ? true : false;
-		boolean portee_basse = (resultat.getHauteur() >= -3 && resultat	.getLargeur() == 0) ? true : false;
-		boolean portee_gauche = (resultat.getLargeur() >= -3 && resultat.getHauteur() == 0) ? true : false;
-		boolean portee_droite = (resultat.getLargeur() <= 3 && resultat.getHauteur() == 0) ? true : false;
-
-		if (portee_haute || portee_basse || portee_gauche || portee_droite) {
-			return true;
-		}
+	@Override
+	public boolean peutTirer() {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	void degat(String cause) {
-		switch (cause) {
-		case "attaque":
-			super.setEnergie(super.getEnergie() - 2);
-			break;
-		case "cible":
-			super.setEnergie(super.getEnergie() - 3);
-			break;
-		case "avance":
-			super.setEnergie(super.getEnergie() - 1);
-			break;
-
-		default:
-			break;
-		}
+	public int getCoutAction() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
-	boolean attaque(Coordonnees c) {
-		return false;
+	public int getCoutDep() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDegatTir() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDegatMine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Coordonnees> getDeplacement() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
