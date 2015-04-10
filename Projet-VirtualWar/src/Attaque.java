d5e2cca464645d945bd595567f2f579ce91ed387
Sawah <rayanlesage@gmail.com>
2015-04-10 10:41:11 +0200

    Update Attaque.java

--- Projet-VirtualWar/src/Attaque.java
+++ Projet-VirtualWar/src/Attaque.java
@@ -9,25 +9,6 @@ public class Attaque extends Action {
 	}
 
 
-	
-
-	@Override
-	public void agit(Robot robot) {
-		// TODO Auto-generated method stub
-		if(this.robot instanceof Char ){
-			robot.setEnergie(robot.getEnergie()-Constante.DEGATCHAR);
-		}
-		else if (this.robot instanceof Tireur){
-			robot.setEnergie(robot.getEnergie()-Constante.DEGATTIREUR);
-		}
-		else if (this.robot instanceof Piegeur){
-			robot.setEnergie(robot.getEnergie()-Constante.DEGATPIEGEUR);
-		}
-	}
-
-
-
-
 	@Override
 	public void agit() {
 		// TODO Auto-generated method stub
