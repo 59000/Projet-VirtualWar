b183c40d3058f61b47447c5b6c522f25afb35220
EiKoZ <quentin.beaudoul-br@laposte.net>
2015-04-14 17:30:23 +0200

    Update Piegeur.java

--- src/Piegeur.java
+++ src/Piegeur.java
@@ -1,7 +1,11 @@
 import java.util.List;
 
 
+<<<<<<< HEAD
 public class Piegeur extends Robot {/*implÃ©menter*/
+=======
+public class Piegeur extends Robot {/*implÃƒÂ©menter*/
+>>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	
 	private List<Coordonnees> coord;
 	int nbMine = Constante.NBMINE;
@@ -11,10 +15,26 @@ public class Piegeur extends Robot {/*implÃ©menter*/
 	}
 
 	@Override
+<<<<<<< HEAD
+	public boolean peutTirer(Coordonnees c) {
+		// TODO Auto-generated method stub
+		Coordonnees resultat = this.getCoord().soustrait(c);
+		boolean portee_haute = (resultat.getHauteur() <=Constante.PORTEEPIEGEUR && resultat.getLargeur() == 0) ? true:false;
+		boolean portee_basse = (resultat.getHauteur() >=-Constante.PORTEEPIEGEUR && resultat.getLargeur() == 0) ? true:false;
+		boolean portee_gauche= (resultat.getLargeur() >=-Constante.PORTEEPIEGEUR && resultat.getHauteur() == 0) ? true:false;
+		boolean portee_droite= (resultat.getLargeur() <=Constante.PORTEEPIEGEUR && resultat.getHauteur() == 0) ? true:false;
+		
+		if (portee_haute||portee_basse||portee_gauche||portee_droite)
+		{
+			return true;
+		}
+			return false;
+=======
 	public boolean peutTirer() {
 		// TODO Auto-generated method stub
 		
 		return false;
+>>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	}
 
 	@Override
