eb1ae225d79117f2e1e459bb024b9514d06e182a
EiKoZ <quentin.beaudoul-br@laposte.net>
2015-04-14 17:28:21 +0200

    Update Char.java

--- src/Char.java
+++ src/Char.java
@@ -1,16 +1,36 @@
 import java.util.List;
 
-public class Char extends Robot {/*implémenter*/
+public class Char extends Robot {/*implÃ©menter*/
 
+<<<<<<< HEAD
+	List<Coordonnees> coord;
+=======
+>>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	/*faire les methodes peutTirer() et getDeplacement()*/
 	public Char(int equipe,int numero) {
 		super(equipe,numero, Constante.ENERGIECHAR);
 	}
 
 	@Override
+<<<<<<< HEAD
+	public boolean peutTirer(Coordonnees c) {
+		// TODO Auto-generated method stub
+		Coordonnees resultat =  super.getCoord().soustrait(c);
+		boolean portee_haute = (resultat.getHauteur() <=Constante.PORTEECHAR && resultat.getLargeur() == 0) ? true:false;
+		boolean portee_basse = (resultat.getHauteur() >=-Constante.PORTEECHAR && resultat.getLargeur() == 0) ? true:false;
+		boolean portee_gauche= (resultat.getLargeur() >=-Constante.PORTEECHAR && resultat.getHauteur() == 0) ? true:false;
+		boolean portee_droite= (resultat.getLargeur() <=Constante.PORTEECHAR && resultat.getHauteur() == 0) ? true:false;
+		
+		if (portee_haute||portee_basse||portee_gauche||portee_droite)
+		{
+			return true;
+		}
+			return false;
+=======
 	public boolean peutTirer() {
 		// TODO Auto-generated method stub
 		return false;
+>>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	}
 
 	@Override
@@ -40,6 +60,10 @@ public class Char extends Robot {/*implémenter*/
 	@Override
 	public List<Coordonnees> getDeplacement() {
 		// TODO Auto-generated method stub
+<<<<<<< HEAD
+		return coord;
+=======
 		return null;
+>>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	}
 }
