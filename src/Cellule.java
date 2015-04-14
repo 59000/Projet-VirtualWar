e5ecd672c5440c20f6bdff454006b8cb4a66cf2e
Sawah <rayanlesage@gmail.com>
2015-04-14 17:34:39 +0200

    Update Cellule.java

--- src/Cellule.java
+++ src/Cellule.java
@@ -6,11 +6,8 @@ public class Cellule extends Coordonnees {
 	 */
 
 	protected int mine;
-<<<<<<< HEAD
 	protected int base;
-=======
 	protected static int base;
->>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	protected Robot robot;
 
 	// String image; //inutile pour le momant
@@ -27,13 +24,11 @@ public class Cellule extends Coordonnees {
 		this.mine = mine;
 	}
 
-<<<<<<< HEAD
 
-=======
 	public int estBase() {
 		return base;
 	}
->>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
+	
 
 	public Coordonnees getCoordonnees() {
 		return new Coordonnees(super.getLargeur(), super.getHauteur());
@@ -71,7 +66,6 @@ public class Cellule extends Coordonnees {
 		 * et la deuxieme en x 0 et y max
 		 */
 		if (b) {
-<<<<<<< HEAD
 			this.base = Constante.BASE1;
 
 		} else if (!b) {
@@ -85,15 +79,6 @@ public class Cellule extends Coordonnees {
 	public int estBase(){
 		return this.base;
 	}
-
-
-=======
-			Cellule.base = Constante.BASE1;
-
-		} else if (!b) {
-			Cellule.base = Constante.BASE2;
-		}
-	}
->>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
-
+	
 }
+
