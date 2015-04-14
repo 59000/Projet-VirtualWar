6e6a278639cc51634958098e473b3b2a52b9660e
Sawah <rayanlesage@gmail.com>
2015-04-14 18:53:27 +0200

    Update Char.java

--- src/Char.java
+++ src/Char.java
@@ -2,12 +2,20 @@ import java.util.List;
 
 public class Char extends Robot {/*implÃ©menter*/
 
+	/**@param coord */
 	List<Coordonnees> coord;
 	/*faire les methodes peutTirer() et getDeplacement()*/
+	
+	/**Cree un Robot Char avec son numero et son equipe
+	 * 
+	 * @param equipe
+	 * @param numero
+	 */
 	public Char(int equipe,int numero) {
 		super(equipe,numero, Constante.ENERGIECHAR);
 	}
-
+	
+	/**Teste si le robot peut tirer sur une certaine cellule*/
 	@Override
 	public boolean peutTirer(Coordonnees c) {
 		// TODO Auto-generated method stub
@@ -23,31 +31,34 @@ public class Char extends Robot {/*implÃ©menter*/
 		}
 			return false;
 			
-
+	}
+	/** Retourne le cout d'action du char*/
 	@Override
 	public int getCoutAction() {
 		// TODO Auto-generated method stub
 		return Constante.COUTTIRERCHAR;
 	}
-
+	/** Retourne le cout de deplacement du char*/
 	@Override
 	public int getCoutDep() {
 		// TODO Auto-generated method stub
 		return Constante.COUTAVANCERCHAR;
 	}
-
+	/**Retourne les degats du char */
 	@Override
 	public int getDegatTir() {
 		// TODO Auto-generated method stub
 		return Constante.DEGATCHAR;
 	}
-
+	/**@return 0 */
 	@Override
 	public int getDegatMine() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	/**Retourne l'ensemble des deplacements pour acceder a une coordonnées
+	 * @return coord;
+	 *  */
 	@Override
 	public List<Coordonnees> getDeplacement() {
 		// TODO Auto-generated method stub
