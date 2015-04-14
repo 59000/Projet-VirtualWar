32a724f431bfe3a268bd0ea1d353d040fc7d39e4
Sawah <rayanlesage@gmail.com>
2015-04-14 18:23:18 +0200

    Update Attaque.java

--- src/Attaque.java
+++ src/Attaque.java
@@ -1,22 +1,17 @@
- 
 public class Attaque extends Action {
-	/*Classe non terminÃ©, la methode agit est censÃ© contenir plus de chose*/
+	/* Classe non terminÃ©, la methode agit est censÃ© contenir plus de chose */
 
-
+	/** Necessaire a la creation de la classe mais pas utile */
 	public Attaque(Robot robot, Coordonnees coord) {
 		super(robot, coord);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
+	/** Permet de faire l'action d'attaque pour un robot*/
 	public void agit() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-
-
-
-	
 }
-
