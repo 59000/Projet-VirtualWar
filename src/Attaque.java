ff4d57102b956cee31a1390a83976d0ff20a0282
Sawah <rayanlesage@gmail.com>
2015-04-14 17:32:54 +0200

    Update Attaque.java

--- src/Attaque.java
+++ src/Attaque.java
@@ -8,10 +8,6 @@ public class Attaque extends Action {
 		// TODO Auto-generated constructor stub
 	}
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	@Override
 	public void agit() {
 		// TODO Auto-generated method stub
