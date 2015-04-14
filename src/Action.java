2dadf41c7e42b0361e0cb963c7cdf8b3498e0e24
Sawah <rayanlesage@gmail.com>
2015-04-14 18:19:57 +0200

    Update Action.java

--- src/Action.java
+++ src/Action.java
@@ -1,25 +1,44 @@
-
 public abstract class Action {
-	/*Il reste a faire la methode getDirection qui renvoit un chemin pour aller a l'objectif*/
-	protected Coordonnees coord ;
-	protected Robot robot ;
-	
-	public Action (Robot robot ,Coordonnees coord){
-		this.robot=robot;
-		this.coord=coord;
+	/*
+	 * Il reste a faire la methode getDirection qui renvoit un chemin pour aller
+	 * a l'objectif
+	 */
+
+	/** @param coord */
+	protected Coordonnees coord;
+	/** @param robot */
+	protected Robot robot;
+
+	/**
+	 * Crée une instance de la classe action en precisant un robot et ses
+	 * coordonnées
+	 * 
+	 * @param robot
+	 * @param coord
+	 */
+	public Action(Robot robot, Coordonnees coord) {
+		this.robot = robot;
+		this.coord = coord;
 	}
-	public  Robot getRobot(){
-		return robot ;
+
+	/** @return robot */
+	public Robot getRobot() {
+		return robot;
 	}
-	
-	/*public Coordonnees getDirection(){
-		return null;
-	}*/
-	
-	public Coordonnees getObjectif(){
+
+	/*
+	 * public Coordonnees getDirection(){ return null; }
+	 */
+	/**
+	 * Retourne une case ciblé par le joueur pour attaquer ou se deplacer
+	 * 
+	 * @return Coordonnees
+	 */
+	public Coordonnees getObjectif() {
 		return new Coordonnees(coord.getLargeur(), coord.getHauteur());
 	}
-	
+
+	/** Abstract qui permet de faire les actions attaques et deplacement */
 	public abstract void agit();
-	
+
 }
