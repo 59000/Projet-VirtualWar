88d08a81eabbed06da9870c2436b758dcc6e93bc
Sawah <rayanlesage@gmail.com>
2015-04-14 19:43:50 +0200

    Update Robot.java

--- src/Robot.java
+++ src/Robot.java
@@ -2,13 +2,23 @@ import java.util.List;
 
 public abstract class Robot {
 	/* implÃ©menter les fonctions prÃ©sentes */
-
+	/**@param coord */
 	protected Coordonnees coord;
+	/**@param equipe */
 	protected int equipe;
+	/**@param energie */
 	protected int energie;
-	protected final int numero;// le numero qui identifie le robot dans l'Ã©quipe
+	/**@param numero */
+	protected final int numero;
+	/**@param cellule*/
 	Cellule cellule;
 
+	/**Cree un robot avec equipe, numero et energie
+	 * 
+	 * @param equipe
+	 * @param numero
+	 * @param energie
+	 */
 	public Robot(int equipe, int numero, int energie) {
 		setCoord(new Coordonnees(0, 0));
 		this.setEquipe(equipe);
@@ -22,31 +32,31 @@ public abstract class Robot {
 			setEnergie(Constante.REGENENBASE);
 		}
 	}	
-
+	/**@return coord*/
 	public Coordonnees getCoord() {
 		return coord;
 	}
-
+	/**Met les coordonnees placees en parametre*/
 	public void setCoord(Coordonnees coord) {
 		this.coord = coord;
 	}
-
+	/**@return energie*/
 	public int getEnergie() {
 		return energie;
 	}
-
+	/**Met l'energie placee en parametre*/
 	public void setEnergie(int energie) {
 		this.energie = energie;
 	}
-
+	/**@return equipe*/
 	public int getEquipe() {
 		return equipe;
 	}
-
+	/**Met l'equipe placee en parametre */
 	public void setEquipe(int equipe) {
 		this.equipe = equipe;
 	}
-
+	/**@return numero*/
 	public int getNumero() {
 		return numero;
 	}
@@ -68,7 +78,7 @@ public abstract class Robot {
 		return "Robot [coord=" + coord + ", equipe=" + equipe + ", numero="
 				+ numero + ", energie=" + energie + "]";
 	}
-
+	/**Gere la perte de point de vie du robot par des tirs */
 	public void subitTir(Robot robot) {
 		if (robot instanceof Char) {
 			setEnergie(getEnergie() - Constante.DEGATCHAR);
@@ -76,7 +86,7 @@ public abstract class Robot {
 			setEnergie(getEnergie() - Constante.DEGATTIREUR);
 		}
 	}
-
+	/**Gere la perte de point de vie du robot par des mines */
 	public void subitMine() {
 		setEnergie(getEnergie() - Constante.DEGATPIEGEUR);
 	}
