7d32ae9ed8a6c6d745d7b16df70ddb86e9578d22
Sawah <rayanlesage@gmail.com>
2015-04-14 19:10:58 +0200

    Update Coordonnees.java

--- src/Coordonnees.java
+++ src/Coordonnees.java
@@ -1,8 +1,15 @@
 public class Coordonnees {
 	
+	/**@param largeur*/
 	private int largeur;
+	/**@param hauteur */
 	private int hauteur;
 
+	/**Crée des Coordoonnees
+	 * 
+	 * @param largeur
+	 * @param hauteur
+	 */
 	public Coordonnees(int largeur, int hauteur) {
 		this.largeur = largeur;
 		this.hauteur = hauteur;
@@ -19,31 +26,31 @@ public class Coordonnees {
 	}
 	
 
-	/** retourne les coordonnÃƒÂ©e sous la forme "[x;y]" */
+	/** retourne les coordonnees sous la forme "[x;y]" */
 	public String toString() {
 		return "[" + this.largeur + ";" + this.hauteur + "]";
 	}
 
-	/** ajoute des coordonnÃ©es aux coordonnÃ©es courantes */
+	/** ajoute des coordonnees aux coordonnees courantes */
 	public Coordonnees ajout(Coordonnees coord) {
 		int l = (this.largeur + coord.getLargeur());
 		int h = (this.hauteur + coord.getHauteur());
 
 		return new Coordonnees(l, h);
 	}
-	
+	/** soustrait des coordonnees aux coordonnees courantes*/
 	public Coordonnees soustrait(Coordonnees coord) {
 		int l = this.largeur - coord.getLargeur();
 		int h = this.hauteur - coord.getHauteur();
 
 		return new Coordonnees(l, h);
-
-
+	}
+	/**Met la hauteur a celle mise en parametre*/
 	public void setHauteur(int i) {
 		// TODO Auto-generated method stub
 		this.hauteur = i;
 	}
-
+	/**Met la Largeur a celle mise en parametre*/
 	public void setLargeur (int i) {
 		// TODO Auto-generated method stub
 		this.largeur = i;
