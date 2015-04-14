24be5f59e2fae4cc8a3d31c0b439b00d659fcfa6
Sawah <rayanlesage@gmail.com>
2015-04-14 17:47:52 +0200

    Update Robot.java

--- src/Robot.java
+++ src/Robot.java
@@ -1,7 +1,6 @@
 import java.util.List;
 
 public abstract class Robot {
-<<<<<<< HEAD
 	/* implÃ©menter les fonctions prÃ©sentes */
 
 	protected Coordonnees coord;
@@ -22,31 +21,7 @@ public abstract class Robot {
 		if(cellule.estBase(this.getCoord())==this.equipe){
 			setEnergie(Constante.REGENENBASE);
 		}
-		// condition foireuse si la base est pas en 0.0 mais comment on rÃ©cupÃ©re les coordonÃ© de la base ?
-		
-=======
-	/*implÃ©menter les fonctions prÃ©sentes*/
-	
-	private Coordonnees coord;
-	private int equipe;
-	private int energie;
-	private final int numero;//le numero qui identifie le robot dans l'Ã©quipe
-
-	public Robot(int equipe,int numero, int energie) {
-		setCoord(new Coordonnees(0, 0));
-		this.setEquipe(equipe);
-		this.setEnergie(energie);
-		this.numero=numero;
-	}
-
-	
-
-	/** augmente l'energie du robot en base de 2 */
-	void regeneration() {
-		setEnergie(getEnergie()+ (this.getCoord().equals(new Coordonnees(0, 0)) ? Constante.REGENENBASE : 0));
-		// condition foireuse si la base est pas en 0.0 mais comment on rÃ©cupÃ©re les coordonÃ© de la base ?
->>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
-	}
+	}	
 
 	public Coordonnees getCoord() {
 		return coord;
@@ -72,7 +47,6 @@ public abstract class Robot {
 		this.equipe = equipe;
 	}
 
-<<<<<<< HEAD
 	public int getNumero() {
 		return numero;
 	}
@@ -105,38 +79,6 @@ public abstract class Robot {
 
 	public void subitMine() {
 		setEnergie(getEnergie() - Constante.DEGATPIEGEUR);
-=======
-	public int getNumero() 
-	{
-		return numero;
-	}
-	abstract public boolean peutTirer();
-	abstract public int getCoutAction();
-	abstract public int getCoutDep();
-	abstract public int getDegatTir();
-	abstract public int getDegatMine();
-	abstract public List<Coordonnees> getDeplacement();
-	
-	
-	@Override
-	public String toString() {
-		return "Robot [coord=" + coord + ", equipe=" + equipe +  ", numero=" + numero + ", energie="
-				+ energie + "]";
-	}
-
-
-
-	public void subitTir(Robot robot ) {
-		if(robot instanceof Char){
-			setEnergie(getEnergie()-Constante.DEGATCHAR);
-		}
-		else if (robot instanceof Tireur){
-			setEnergie(getEnergie()-Constante.DEGATTIREUR);
-		}
-	}
-	public void subitMine() {
-		setEnergie(getEnergie()-Constante.DEGATPIEGEUR);
->>>>>>> b06ce68ea1a3d83492f6585fad7070a9e5a1fc7c
 	}
 
 }
