bae25bac0803f47cca756ff1bffa97568d7e061e
LapinZinski <LapinZinski@users.noreply.github.com>
2015-04-16 07:49:34 +0200

    Le déplacement de la dernière chance

--- src/Deplacement.java
+++ src/Deplacement.java
@@ -1,21 +1,10 @@
 public class Deplacement extends Action {
-	/* Il faut finir la methode agit */
 	/**Deplace le robot selon sa portée de deplacement*/
 	public Deplacement(Robot robot, Coordonnees coord) {
 		super(robot, coord);
-		
-		int porte = 1;
-		
-		if (this.robot instanceof Char) {
-			porte = Constante.DEPLACEMENTCHAR;
-		} else if (this.robot instanceof Tireur) {
-			porte = Constante.DEPLACEMENTTIREUR;
-		} else if (this.robot instanceof Piegeur) {
-			porte = Constante.DEPLACEMENTPIEGEUR;
-		}
-		
-		this.coord.setHauteur((int) this.coord.getHauteur()*porte);
-		this.coord.setLargeur((int) this.coord.getLargeur()*porte);
+	
+		this.coord.setHauteur((int) this.coord.getHauteur());
+		this.coord.setLargeur((int) this.coord.getLargeur());
 		
 		this.agit();
 	}
@@ -34,19 +23,5 @@ public class Deplacement extends Action {
 			this.robot.setEnergie(this.robot.getEnergie()+Constante.COUTAVANCERPIEGEUR);
 		}
 
-	}
-	
-	/** Si Char bloquée, reviens à un déplacement simple */
-	public static Robot charBloquee(Robot r, Coordonnees coord){
-		if (coord.equals(Constante.HAUT)) {
-			r.getCoord().setHauteur(r.getCoord().getHauteur() + 1);
-		} else if (coord.equals(Constante.BAS)) {
-			r.getCoord().setHauteur(r.getCoord().getHauteur() - 1);
-		} else if (coord.equals(Constante.GAUCHE)) {
-			r.getCoord().setLargeur(r.getCoord().getLargeur() + 1);
-		} else if (coord.equals(Constante.DROIT)) {
-			r.getCoord().setLargeur(r.getCoord().getLargeur() - 1);
-		}
-		return r;
 	}
 }
