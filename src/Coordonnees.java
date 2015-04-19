8dd21ee6d0f4cd9d30751a3e53254a646f5e1f85
Sawah <rayanlesage@gmail.com>
2015-04-19 22:27:55 +0200

    Update Coordonnees.java

--- src/Coordonnees.java
+++ src/Coordonnees.java
@@ -55,7 +55,7 @@ public class Coordonnees {
 		// TODO Auto-generated method stub
 		this.largeur = i;
 	}
-	/** Vérifie que deux coordonées sont égaux */
+	/** Vérifie que deux coordonées sont egaux */
 	public boolean equals(Coordonnees coord) {
 		// TODO Auto-generated method stub
 		return (this.hauteur == coord.hauteur && this.largeur == coord.largeur);
