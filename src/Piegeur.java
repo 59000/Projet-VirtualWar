c2ca362e731691ad68ac0a2513eaf1aa5e5674af
RST-59 <RST-59@users.noreply.github.com>
2015-04-19 22:09:25 +0200

    Update Piegeur.java

--- src/Piegeur.java
+++ src/Piegeur.java
@@ -1,12 +1,12 @@
 import java.util.List;
 
-public class Piegeur extends Robot {/*implÃƒÂ©menter*/
+public class Piegeur extends Robot {/*implÃƒÆ’Ã‚Â©menter*/
 	/**@param coord */
 	private List<Coordonnees> coord;
 	/**@param NBMINE */
 	int nbMine = Constante.NBMINE;
 	/*faire les methodes peutTirer() et getDeplacement()*/
-	/**Crée un Robot piegeur avec son numéro et son equipe */
+	/**CrÃ©e un Robot piegeur avec son numÃ©ro et son equipe */
 	public Piegeur(int equipe,int numero) {
 		super(equipe,numero, Constante.ENERGIEPIEGEUR);
 	}
@@ -50,7 +50,7 @@ public class Piegeur extends Robot {/*implÃƒÂ©menter*/
 		// TODO Auto-generated method stub
 		return Constante.DEGATPIEGEUR;
 	}
-	/**Retourne l'ensemble des deplacements pour acceder a une coordonnées
+	/**Retourne l'ensemble des deplacements pour acceder a une coordonnÃ©es
 	 * @return coord;
 	 *  */
 	@Override
@@ -64,6 +64,6 @@ public class Piegeur extends Robot {/*implÃƒÂ©menter*/
 		nbMine = Constante.NBMINE;
 	}
 	public String toString(){
-		return "Piegeur" +super.toString()+"mines="+nbMine;
+		return "Piegeur" +super.toString()+" mines="+nbMine;
 	}
 }
