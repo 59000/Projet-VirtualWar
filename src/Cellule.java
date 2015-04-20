ea6d73c775a4856137e8272621608f04e67454f5
Sawah <rayanlesage@gmail.com>
2015-04-20 23:27:00 +0200

    Update Cellule.java

--- src/Cellule.java
+++ src/Cellule.java
@@ -1,35 +1,119 @@
-package Action;
+package Plateau;
+import java.util.Random;
+
 import Constante.Constante;
-import Plateau.Coordonnees;
-import Robot.Char;
-import Robot.Piegeur;
 import Robot.Robot;
-import Robot.Tireur;
 
-public class Deplacement extends Action {
-	/**Deplace le robot selon sa portÃ©e de deplacement*/
-	public Deplacement(Robot robot, Coordonnees coord) {
-		super(robot, coord);
-	
-		this.coord.setHauteur((int) this.coord.getHauteur());
-		this.coord.setLargeur((int) this.coord.getLargeur());
-		
-		this.agit();
+public class Cellule extends Coordonnees {
+
+	/*
+	 * essayer de corriger les methodes deplacerSur ainsi que viderCase et le
+	 * void ajout
+	 */
+	/** @param int mine */
+	protected int mine;
+	/** @param int base */
+	public int base;
+	/**
+	 * @param Robot
+	 *            robot
+	 */
+	protected Robot robot;
+	/** @param pourcentage */
+	double pourcentage = Constante.POURCENTAGEOBSTACLE;
+	/** @param ran */
+	Random ran = new Random();
+	boolean obstacle = false;
+
+	// String image; //inutile pour le momant
+	/**
+	 * Cree une instance de la classe Cellule en precisant ses coordonnees
+	 * 
+	 * @param largeur
+	 * @param hauteur
+	 */
+	public Cellule(int largeur, int hauteur) {
+		super(largeur, hauteur);
 	}
-	
-	/**Methode qui permet le deplacement du robot appelÃ© par le constructeur */
+
+	/** @return mine */
+	public int contienMine() {
+		return this.mine;
+	}
+
+	/** Remettre le nombre de mine au nombre desire */
+	public void setMine(int mine) {
+		this.mine = mine;
+	}
+
+	/** @return new Coordonnees */
+	public Coordonnees getCoordonnees() {
+		return new Coordonnees(super.getLargeur(), super.getHauteur());
+	}
+
+	/** @return robot */
+	public Robot getContenu() {
+		return robot;
+	}
+
 	@Override
-	public void agit() {
-		if (this.robot instanceof Char) {
-			this.robot.setCoord(this.robot.getCoord().ajout(coord));
-			this.robot.setEnergie(this.robot.getEnergie()+Constante.COUTAVANCERCHAR);
-		} else if (this.robot instanceof Tireur) {
-			this.robot.setCoord(this.robot.getCoord().ajout(coord));
-			this.robot.setEnergie(this.robot.getEnergie()+Constante.COUTAVANCERTIREUR);
-		} else  if (this.robot instanceof Piegeur) {
-			this.robot.setCoord(this.robot.getCoord().ajout(coord));
-			this.robot.setEnergie(this.robot.getEnergie()+Constante.COUTAVANCERPIEGEUR);
-		}
+	public String toString() {
+		return "Cellule [mine=" + mine + ", base=" + base /* + ", image=" + image */
+				+ ", robot=" + robot + " coord" + super.toString() + "]";
+	}
+
+	/** Deplace le robot sur la celulle actuelle */
+	public void deplaceSur(Robot robot) {
+		// if (robot.getCoord() == this.getCoordonnees()) {
+		this.robot = robot;
+		// }
 
 	}
+
+	/** Permet de vider la case actuelle */
+	public void videCase() {
+		this.mine = 0;
+		this.robot = null;
+	}
+
+	/** Ajoute un robot a l'equipe */
+	public void ajout(int equipe) {
+		this.robot.setEquipe(equipe);
+	}
+
+	/** Permet de CrÃƒÆ’Ã‚Â©e une base */
+	void creeBase(boolean b) {
+
+		if (b) {
+			this.base = Constante.BASE1;
+
+		} else if (!b) {
+			this.base = Constante.BASE2;
+		}
+	}
+
+	/** Teste si la cellule est une base en fonction des coordonnees */
+	public int estBase(Coordonnees c) {
+		return this.base;
+
+	}
+
+	/** Teste si la cellule actuelle est une base */
+	public int estBase() {
+		return this.base;
+	}
+
+	/** Verifie si la case actuelle est un obstacle */
+	public boolean estObstacle() {
+		return obstacle;
+	}
+
+	public void cree_Obstacle() {
+		if (this.ran.nextInt(100) < pourcentage) {
+			obstacle = true;
+		} else {
+			obstacle = false;
+		}
+	}
+
 }
