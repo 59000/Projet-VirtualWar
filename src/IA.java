4511cac9ee7351bf8d83da9727b682ef731d8b7b
RST-59 <RST-59@users.noreply.github.com>
2015-04-27 14:46:02 +0200

    IA
    abstract pouur des futur IA prétes

---
+++ src/IA.java
@@ -0,0 +1,25 @@
+package ia;
+
+import Robot.Piegeur;
+import Robot.Robot;
+
+public abstract class IA 
+{
+	int equipe;
+	Robot[] robots;
+	
+	public IA(int equipe)
+	{
+		this.equipe=equipe;
+	}
+	
+	abstract Robot[] constitution_equipes(int nb_robot);
+	/** @return 1 deplacement,2 attaque*/
+	abstract int selection_action();
+	abstract Robot selection_robot_actif();
+	abstract String selection_direction_deplacement(Robot robot);
+	abstract String selection_direction_attaque(Piegeur p);
+	/**@return numero du robot cible*/
+	abstract int selection_robot_cible(Robot robot);
+}
+
