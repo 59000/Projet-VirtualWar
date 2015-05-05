c916845c6f32cf5b979e6ae5d034a743d103bdab
Sawah <rayanlesage@gmail.com>
2015-05-05 17:12:09 +0200

    Update IA.java

--- src/IA.java
+++ src/IA.java
@@ -1,5 +1,6 @@
 package ia;
 
+import Plateau.Coordonnees;
 import Robot.Piegeur;
 import Robot.Robot;
 
@@ -17,8 +18,8 @@ public abstract class IA
 	/** @return 1 deplacement,2 attaque*/
 	abstract int selection_action();
 	abstract Robot selection_robot_actif();
-	abstract String selection_direction_deplacement(Robot robot);
-	abstract String selection_direction_attaque(Piegeur p);
+	abstract Coordonnees selection_direction_deplacement(Robot robot);
+	abstract Coordonnees selection_direction_attaque(Piegeur p);
 	/**@return numero du robot cible*/
 	abstract int selection_robot_cible(Robot robot);
 }
