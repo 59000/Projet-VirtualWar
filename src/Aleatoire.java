1c71b26f1dda12950148baf87e9e2f98e858b7a4
Sawah <rayanlesage@gmail.com>
2015-05-11 11:33:16 +0200

    Update Aleatoire.java

--- src/Aleatoire.java
+++ src/Aleatoire.java
@@ -9,10 +9,20 @@ import Robot.Tireur;
 public class Aleatoire extends IA {
 	Random r = new Random();
 
+	/**
+	 * Cree une instance d equipe aleatoire
+	 * 
+	 * @param equipe
+	 */
 	public Aleatoire(int equipe) {
 		super(equipe);
 	}
 
+	/**
+	 * Constitue aleatoirement les equipe avec un nombre de robot
+	 * 
+	 * @param nb_robot
+	 */
 	@Override
 	public Robot[] constitution_equipes(int nb_robot) {
 		Robot[] areturn = new Robot[nb_robot];
@@ -33,11 +43,13 @@ public class Aleatoire extends IA {
 		return areturn;
 	}
 
+	/** Selection d action : soit deplacement , soit attaque */
 	@Override
 	public int selection_action() {
 		return (r.nextInt(2) + 1);
 	}
 
+	/** Selection du robot qui va jouer dans equipe aleatoirement */
 	@Override
 	public Robot selection_robot_actif() {
 		int nbRobot = r.nextInt(robots.length);
@@ -47,6 +59,9 @@ public class Aleatoire extends IA {
 		return robots[nbRobot];
 	}
 
+	/**
+	 * Selection de la direction du deplacement du robot si selection_action = 1
+	 */
 	@Override
 	public String selection_direction_deplacement(Robot robot) {
 		if (robot instanceof Char) {
@@ -83,11 +98,16 @@ public class Aleatoire extends IA {
 		return null;// compilateur autoritaire
 	}
 
+	/**
+	 * Selection de la direction de lattaque si selection_action = 2 et que le
+	 * robot est un Piegeur
+	 */
 	@Override
 	public String selection_direction_attaque(Piegeur p) {
 		return selection_direction_deplacement(p);
 	}
 
+	/** Selection de la direction de lattaque si selection_action = 2 */
 	@Override
 	public int selection_robot_cible(Robot robot) {
 		return r.nextInt(robots.length);
