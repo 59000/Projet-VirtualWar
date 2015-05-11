0dd3c96e14f09a981fcc265874d7f91320b55fc9
Sawah <rayanlesage@gmail.com>
2015-05-11 11:35:46 +0200

    Update IA.java

--- src/IA.java
+++ src/IA.java
@@ -3,24 +3,24 @@ package ia;
 import Robot.Piegeur;
 import Robot.Robot;
 
-public abstract class IA 
-{
+public abstract class IA {
 	int equipe;
 	Robot[] robots;
-	
-	public IA(int equipe)
-	{
-		this.equipe=equipe;
+	/**Cree une instance IA en fonction de equipe
+	 * @param equipe */
+	public IA(int equipe) {
+		this.equipe = equipe;
 	}
-	
+
 	public abstract Robot[] constitution_equipes(int nb_robot);
-	/** @return 1 deplacement,2 attaque*/
+
 	public abstract int selection_action();
+
 	public abstract Robot selection_robot_actif();
+
 	public abstract String selection_direction_deplacement(Robot robot);
+
 	public abstract String selection_direction_attaque(Piegeur p);
-	/**@return numero du robot cible*/
+
 	public abstract int selection_robot_cible(Robot robot);
 }
-
-
