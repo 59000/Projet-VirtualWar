415988cbb13406b593ba6efc39e9f82496626080
EiKoZ <quentin.beaudoul-br@laposte.net>
2015-05-26 22:06:35 +0200

    Update ConfigEquipePvP.java

--- src/ConfigEquipePvP.java
+++ src/ConfigEquipePvP.java
@@ -9,6 +9,7 @@ import java.awt.GridLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ConfigEquipePvP {
 
@@ -25,49 +26,67 @@ public class ConfigEquipePvP {
 		fen.setLayout(new GridBagLayout());
 		fen.setContentPane(new PanelFond());
 		
-		
+		/** panneau principal*/
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1,2));
 		panel.setPreferredSize(new Dimension(700,500));
 		panel.setBackground(new Color(0,0,0,0));	
 		panel.setOpaque(false);
-		
+		/** panneau gauche*/
 		JPanel panGauche = new JPanel();
 		panGauche.setLayout(new BorderLayout());
 		panGauche.setPreferredSize(new Dimension(350,500));
 		panGauche.setBackground(new Color(0,0,0,0));
 		panGauche.setOpaque(false);
 		
-		
+		/**titre set nom equipe 1*/
 		JPanel equipeGauche = new JPanel();
-		equipeGauche.setLayout(new BorderLayout());
-		equipeGauche.setPreferredSize(new Dimension(200,50));
-		equipeGauche.setBackground(Color.GREEN);
-		panGauche.add(equipeGauche,BorderLayout.NORTH);
+		equipeGauche.setLayout(new GridLayout(5,1));
+		equipeGauche.setPreferredSize(new Dimension(200,100));
+		//equipeGauche.setBackground(new Color(0,0,0,0));
+		//equipeGauche.setOpaque(false);
+		equipeGauche.setBackground(Color.green);
 		
 		
 		JLabel labelTitreGauche = new JLabel("Equipe 1 ",JLabel.CENTER);
+		labelTitreGauche.setForeground(Color.LIGHT_GRAY);
 		
-		equipeGauche.add(labelTitreGauche);
+		/**textfield equipe 1*/
+		
+		JTextField textNomEquipe1 = new JTextField("Equipe 1");
+		textNomEquipe1.setPreferredSize(new Dimension(50,5));
+		
+		textNomEquipe1.setVisible(true);
+		equipeGauche.add(textNomEquipe1, BorderLayout.CENTER);
+		
+		
+		
+		panGauche.add(equipeGauche,BorderLayout.NORTH);
+		equipeGauche.add(labelTitreGauche, BorderLayout.NORTH);
 		
 		
 
-
+		/** panneau droite*/
 		JPanel panDroit = new JPanel();
 		panDroit.setLayout(new BorderLayout());
 		panDroit.setPreferredSize(new Dimension(350,500));
 		panDroit.setBackground(new Color(0,0,0,0));
 		panDroit.setOpaque(false);
-		
+		/**set nom equipe 2*/
 		JPanel equipeDroit = new JPanel();
 		equipeDroit.setLayout(new BorderLayout());
-		equipeDroit.setPreferredSize(new Dimension(200,50));
-		equipeDroit.setBackground(Color.RED);
-		
+		equipeDroit.setPreferredSize(new Dimension(200,80));
+		equipeDroit.setBackground(new Color(0,0,0,0));
+		equipeDroit.setOpaque(false);
 		panDroit.add(equipeDroit,BorderLayout.NORTH);
 		
 		
+		JLabel labelTitreDroite = new JLabel("Equipe 2 ",JLabel.CENTER);
+		labelTitreDroite.setForeground(Color.LIGHT_GRAY);
 		
+		
+		equipeDroit.add(labelTitreDroite);
+		panDroit.add(equipeDroit,BorderLayout.NORTH);
 		panel.add(panGauche);
 		panel.add(panDroit);
 		
