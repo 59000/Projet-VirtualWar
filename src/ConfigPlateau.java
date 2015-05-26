a6da36a99d2947d814a3b8440ca926ec20d4ec59
EiKoZ <quentin.beaudoul-br@laposte.net>
2015-05-26 12:54:11 +0200

    Update ConfigPlateau.java

--- src/ConfigPlateau.java
+++ src/ConfigPlateau.java
@@ -3,29 +3,30 @@ package Menu;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
-import java.util.concurrent.BrokenBarrierException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ConfigPlateau {
 
 	public ConfigPlateau() {
 
-		final JFrame fen = new JFrame("VirtalWar");
-		JPanel panHautGauche = new JPanel(new GridLayout());
+		final JFrame fen = new JFrame("VirtualWar");
+		JPanel panHautGauche = new JPanel(new GridLayout(1,4));
 		JPanel pandroite = new JPanel();
 		Dimension ecran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 		JButton petit = new JButton("Petit");
+		petit.setPreferredSize(new Dimension(100,50));
 		JButton grand = new JButton("Grand");
+		grand.setPreferredSize(new Dimension(100,50));
 		JButton degage = new JButton("Degage");
+		degage.setPreferredSize(new Dimension(100,50));
 		JButton encombre = new JButton("Encombre");
+		encombre.setPreferredSize(new Dimension(100,50));
 		int hauteur_ecran = (int) ecran.getHeight();
 		int largeur_ecran = (int) ecran.getWidth();
 
@@ -36,10 +37,13 @@ public class ConfigPlateau {
 		fen.setResizable(false);
 		fen.setLayout(new GridBagLayout());
 		fen.setContentPane(new PanelFond());
+		
+		JLabel lab = new JLabel("Configuration Rapide :");
+		lab.setForeground(Color.lightGray);
 
 		/** panneau hautgauche */
-		panHautGauche.setPreferredSize(new Dimension(500 / 2, 500 / 2));
-
+		panHautGauche.setPreferredSize(new Dimension(400, 50));
+		
 		panHautGauche.add(petit);
 		panHautGauche.add(grand);
 		panHautGauche.add(degage);
@@ -47,12 +51,13 @@ public class ConfigPlateau {
 
 		/** panneau droit */
 
-		pandroite.setPreferredSize(new Dimension(500 / 2, 500 / 2));
+		pandroite.setPreferredSize(new Dimension(250,250));
 		pandroite.setBackground(Color.GREEN);
 
-		/** visibilité */
-		fen.getContentPane().add(pandroite);
+		/** visibilitÃ© */
+		fen.getContentPane().add(lab, BorderLayout.NORTH);
 		fen.getContentPane().add(panHautGauche);
+		fen.getContentPane().add(pandroite);
 		fen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		fen.pack();
 		fen.setVisible(true);
