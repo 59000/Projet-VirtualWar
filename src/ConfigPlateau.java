e5974caf97e5c751434c9df3bc4eca983357809e
Sawah <rayanlesage@gmail.com>
2015-05-26 13:27:00 +0200

    Update ConfigPlateau.java

--- src/ConfigPlateau.java
+++ src/ConfigPlateau.java
@@ -26,14 +26,13 @@ public class ConfigPlateau {
 		JButton degage = new JButton("Degage");
 		degage.setPreferredSize(new Dimension(100,50));
 		JButton encombre = new JButton("Encombre");
-		encombre.setPreferredSize(new Dimension(100,50));
+		encombre.setPreferredSize(new Dimension(130,50));
 		int hauteur_ecran = (int) ecran.getHeight();
 		int largeur_ecran = (int) ecran.getWidth();
 
 		fen.setLocation((int) (largeur_ecran * 0.20),
 				(int) (hauteur_ecran * 0.30));
 		fen.setPreferredSize(new Dimension(700, 500));
-		fen.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		fen.setResizable(false);
 		fen.setLayout(new GridBagLayout());
 		fen.setContentPane(new PanelFond());
@@ -42,7 +41,7 @@ public class ConfigPlateau {
 		lab.setForeground(Color.lightGray);
 
 		/** panneau hautgauche */
-		panHautGauche.setPreferredSize(new Dimension(400, 50));
+		panHautGauche.setPreferredSize(new Dimension(500, 50));
 		
 		panHautGauche.add(petit);
 		panHautGauche.add(grand);
@@ -52,6 +51,8 @@ public class ConfigPlateau {
 		/** panneau droit */
 
 		pandroite.setPreferredSize(new Dimension(250,250));
+		
+	
 		pandroite.setBackground(Color.GREEN);
 
 		/** visibilitÃ© */
