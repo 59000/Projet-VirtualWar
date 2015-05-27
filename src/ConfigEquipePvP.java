64fd8025d2b4c209721020eb63ac92ba27cd677a
Sawah <rayanlesage@gmail.com>
2015-05-27 20:05:49 +0200

    Update ConfigEquipePvP.java

--- src/ConfigEquipePvP.java
+++ src/ConfigEquipePvP.java
@@ -13,6 +13,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -100,46 +101,50 @@ public class ConfigEquipePvP {
 
 		/** Combobox equipe gauche */
 
-		JComboBox<String> gaucheR0 = new JComboBox<String>();
+		JComboBox<Robot> gaucheR0 = new JComboBox<Robot>();
 
 		((JLabel) gaucheR0.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		gaucheR0.addItem("------------");
-		gaucheR0.addItem("Tireur");
-		gaucheR0.addItem("Piegeur");
-		gaucheR0.addItem("Char");
+		gaucheR0.addItem(null);
+		gaucheR0.addItem(new Tireur(0, 0));
+		gaucheR0.addItem(new Piegeur(0, 0));
+		gaucheR0.addItem(new Char(0, 0));
 
-		JComboBox<String> gaucheR1 = new JComboBox<String>();
+		JComboBox<Robot> gaucheR1 = new JComboBox<Robot>();
+
 		((JLabel) gaucheR1.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		gaucheR1.addItem("------------");
-		gaucheR1.addItem("Tireur");
-		gaucheR1.addItem("Piegeur");
-		gaucheR1.addItem("Char");
+		gaucheR1.addItem(null);
+		gaucheR1.addItem(new Tireur(0, 1));
+		gaucheR1.addItem(new Piegeur(0, 1));
+		gaucheR1.addItem(new Char(0, 1));
 
-		JComboBox<String> gaucheR2 = new JComboBox<String>();
+		JComboBox<Robot> gaucheR2 = new JComboBox<Robot>();
+
 		((JLabel) gaucheR2.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		gaucheR2.addItem("------------");
-		gaucheR2.addItem("Tireur");
-		gaucheR2.addItem("Piegeur");
-		gaucheR2.addItem("Char");
+		gaucheR2.addItem(null);
+		gaucheR2.addItem(new Tireur(0, 2));
+		gaucheR2.addItem(new Piegeur(0, 2));
+		gaucheR2.addItem(new Char(0, 2));
 
-		JComboBox<String> gaucheR3 = new JComboBox<String>();
+		JComboBox<Robot> gaucheR3 = new JComboBox<Robot>();
+
 		((JLabel) gaucheR3.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		gaucheR3.addItem("------------");
-		gaucheR3.addItem("Tireur");
-		gaucheR3.addItem("Piegeur");
-		gaucheR3.addItem("Char");
+		gaucheR3.addItem(null);
+		gaucheR3.addItem(new Tireur(0, 3));
+		gaucheR3.addItem(new Piegeur(0, 3));
+		gaucheR3.addItem(new Char(0, 3));
 
-		JComboBox<String> gaucheR4 = new JComboBox<String>();
+		JComboBox<Robot> gaucheR4 = new JComboBox<Robot>();
+
 		((JLabel) gaucheR4.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		gaucheR4.addItem("------------");
-		gaucheR4.addItem("Tireur");
-		gaucheR4.addItem("Piegeur");
-		gaucheR4.addItem("Char");
+		gaucheR4.addItem(null);
+		gaucheR4.addItem(new Tireur(0, 4));
+		gaucheR4.addItem(new Piegeur(0, 4));
+		gaucheR4.addItem(new Char(0, 4));
 
 		equipeGauche.add(textNomEquipe1);
 		panGauche.add(equipeGauche, BorderLayout.NORTH);
@@ -165,7 +170,7 @@ public class ConfigEquipePvP {
 		 * equipeDroit.setBackground(new Color(0,0,0,0));
 		 * equipeDroit.setOpaque(false);
 		 */
-		equipeDroit.setBackground(Color.YELLOW);
+		equipeDroit.setBackground(new Color(255,90,0));
 		panDroit.add(equipeDroit, BorderLayout.NORTH);
 
 		JLabel labelTitreDroite = new JLabel("Equipe 2 ", JLabel.CENTER);
@@ -215,46 +220,50 @@ public class ConfigEquipePvP {
 
 		/** Combobox equipe droit */
 
-		JComboBox<String> droitR0 = new JComboBox<String>();
+		JComboBox<Robot> droitR0 = new JComboBox<Robot>();
 
 		((JLabel) droitR0.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		droitR0.addItem("------------");
-		droitR0.addItem("Tireur");
-		droitR0.addItem("Piegeur");
-		droitR0.addItem("Char");
+		droitR0.addItem(null);
+		droitR0.addItem(new Tireur(1, 0));
+		droitR0.addItem(new Piegeur(1, 0));
+		droitR0.addItem(new Char(1, 0));
 
-		JComboBox<String> droitR1 = new JComboBox<String>();
+		JComboBox<Robot> droitR1 = new JComboBox<Robot>();
+
 		((JLabel) droitR1.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		droitR1.addItem("------------");
-		droitR1.addItem("Tireur");
-		droitR1.addItem("Piegeur");
-		droitR1.addItem("Char");
+		droitR1.addItem(null);
+		droitR1.addItem(new Tireur(1, 1));
+		droitR1.addItem(new Piegeur(1, 1));
+		droitR1.addItem(new Char(1, 1));
 
-		JComboBox<String> droitR2 = new JComboBox<String>();
+		JComboBox<Robot> droitR2 = new JComboBox<Robot>();
+
 		((JLabel) droitR2.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		droitR2.addItem("------------");
-		droitR2.addItem("Tireur");
-		droitR2.addItem("Piegeur");
-		droitR2.addItem("Char");
+		droitR2.addItem(null);
+		droitR2.addItem(new Tireur(1, 2));
+		droitR2.addItem(new Piegeur(1, 2));
+		droitR2.addItem(new Char(1, 2));
 
-		JComboBox<String> droitR3 = new JComboBox<String>();
+		JComboBox<Robot> droitR3 = new JComboBox<Robot>();
+
 		((JLabel) droitR3.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		droitR3.addItem("------------");
-		droitR3.addItem("Tireur");
-		droitR3.addItem("Piegeur");
-		droitR3.addItem("Char");
+		droitR3.addItem(null);
+		droitR3.addItem(new Tireur(1, 3));
+		droitR3.addItem(new Piegeur(1, 3));
+		droitR3.addItem(new Char(1, 3));
 
-		JComboBox<String> droitR4 = new JComboBox<String>();
+		JComboBox<Robot> droitR4 = new JComboBox<Robot>();
+
 		((JLabel) droitR4.getRenderer())
 				.setHorizontalAlignment(SwingConstants.CENTER);
-		droitR4.addItem("------------");
-		droitR4.addItem("Tireur");
-		droitR4.addItem("Piegeur");
-		droitR4.addItem("Char");
+		droitR4.addItem(null);
+		droitR4.addItem(new Tireur(1, 4));
+		droitR4.addItem(new Piegeur(1, 4));
+		droitR4.addItem(new Char(1, 4));
 
 		panDroit.add(equipeDroit, BorderLayout.NORTH);
 		equipeDroit.add(textNomEquipe2);
@@ -304,11 +313,71 @@ public class ConfigEquipePvP {
 				 * Mettre toutes les conditions pour jouer : mettre un nom
 				 * dequipe, avoir le meme nombre de robot de chaque cote
 				 */
+				int cptEquipe1 = 0;
+				int cptEquipe2 = 0;
 
-				if (e.getButton() == MouseEvent.BUTTON1) {
+				Robot[][] equipe = new Robot[2][5];
+				if (gaucheR0.getSelectedItem() != null) {
+					equipe[0][0] = (Robot) gaucheR0.getSelectedItem();
+					cptEquipe1++;
+				}
+				if (gaucheR1.getSelectedItem() != null) {
+					equipe[0][1] = (Robot) gaucheR1.getSelectedItem();
+					cptEquipe1++;
+				}
+				if (gaucheR2.getSelectedItem() != null) {
+					equipe[0][2] = (Robot) gaucheR2.getSelectedItem();
+					cptEquipe1++;
+				}
+				if (gaucheR3.getSelectedItem() != null) {
+					equipe[0][3] = (Robot) gaucheR3.getSelectedItem();
+					cptEquipe1++;
+				}
+				if (gaucheR4.getSelectedItem() != null) {
+					equipe[0][4] = (Robot) gaucheR4.getSelectedItem();
+					cptEquipe1++;
+				}
+
+				if (droitR0.getSelectedItem() != null) {
+					equipe[1][0] = (Robot) droitR0.getSelectedItem();
+					cptEquipe2++;
+				}
+				if (droitR1.getSelectedItem() != null) {
+					equipe[1][1] = (Robot) droitR1.getSelectedItem();
+					cptEquipe2++;
+				}
+				if (droitR2.getSelectedItem() != null) {
+					equipe[1][2] = (Robot) droitR2.getSelectedItem();
+					cptEquipe2++;
+				}
+				if (droitR3.getSelectedItem() != null) {
+					equipe[1][3] = (Robot) droitR3.getSelectedItem();
+					cptEquipe2++;
+				}
+				if (droitR4.getSelectedItem() != null) {
+					equipe[1][4] = (Robot) droitR4.getSelectedItem();
+					cptEquipe2++;
+				}
+
+				if (cptEquipe1 == cptEquipe2
+						&& !textNomEquipe1.getText().equals(
+								"Equipe 1 : Entrez un nom ")
+						&& !textNomEquipe1.getText().equals("")
+						&& !textNomEquipe2.getText().equals(
+								"Equipe 2 : Entrez un nom ")
+						&& !textNomEquipe2.getText().equals("")
+						&& cptEquipe1 != 0
+						&& cptEquipe2 != 0
+						&& !textNomEquipe1.getText().equals(
+								textNomEquipe2.getText())) {
 					fen.dispose();
 					new TestGraphic();
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Nom incorrect et/ou equilibrer les equipes !");
+
 				}
+
 			}
 		});
 
@@ -344,7 +413,7 @@ public class ConfigEquipePvP {
 
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				// TODO Auto-generated method stub
+
 				fen.dispose();
 				new ConfigPlateau();
 			}
